c5b2efd78fed6326ef3665589a43cd7877cfecd7
shubhamkmehta <shubhamarya.4596@gmail.com>
2026-03-05 00:58:30 +0530

    adding userService

---
+++ userService/src/main/java/com/example/entities/UserInfoDto.java
@@ -0,0 +1,31 @@
+package com.example.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserInfoDto {
+
+    Long id;
+
+    @Id
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long phoneNumber;
+    private String profilePicture;
+
+}
